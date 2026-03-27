c7713f372ed7ed1b4406fc79cd83dadfadef20d7
eafalkens <alicia-fs@outlook.com>
2026-03-27 10:50:57 +0100

    Initial commit

---
+++ src/main/java/org/group1/projectbackend/ProjectBackendGroup1Application.java
@@ -0,0 +1,13 @@
+package org.group1.projectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBackendGroup1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBackendGroup1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/group1/projectbackend/ProjectBackendGroup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.group1.projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackendGroup1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
