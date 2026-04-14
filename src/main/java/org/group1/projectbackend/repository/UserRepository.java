437a14c2626a6db067d4a6b6de1ecb578d49233b
Johan <Johan.Jansson@iths.se>
2026-04-14 15:45:43 +0200

    resolve merge conflicts with main

--- src/main/java/org/group1/projectbackend/repository/UserRepository.java
+++ src/main/java/org/group1/projectbackend/repository/UserRepository.java
@@ -2,11 +2,6 @@ package org.group1.projectbackend.repository;
 
 import org.group1.projectbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-=======
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -18,4 +13,3 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 }
->>>>>>> main
