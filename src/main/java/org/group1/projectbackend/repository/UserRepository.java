6e704f1087f5236245fad7e1b92f058f72305220
eafalkens <alicia-fs@outlook.com>
2026-04-14 13:49:38 +0200

    fix: clean merge conflict in UserRepository

--- src/main/java/org/group1/projectbackend/repository/UserRepository.java
+++ src/main/java/org/group1/projectbackend/repository/UserRepository.java
@@ -9,21 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-<<<<<<< HEAD
-}
-import java.util.Optional;
-import org.group1.projectbackend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUsername(String username);
 }
-=======
-    Optional<User> findByEmail(String email);
-
-    Optional<User> findByUsername(String username);
-}
->>>>>>> 8dc279c (fix: merge conflict in UserRepository)
