21dc7269f85bf196d949c64e7013f4b0b2b3dd09
eafalkens <alicia-fs@outlook.com>
2026-04-16 08:45:35 +0200

    refactor: add comments to UserRepository

--- src/main/java/org/group1/projectbackend/repository/UserRepository.java
+++ src/main/java/org/group1/projectbackend/repository/UserRepository.java
@@ -9,7 +9,10 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // Returns a user by email if it exists
     Optional<User> findByEmail(String email);
 
+    // Returns a user by username if it exists
     Optional<User> findByUsername(String username);
+
 }
