fb6097b2feb3a6908362c74eca38ec2c07f3b1f6
eafalkens <alicia-fs@outlook.com>
2026-04-16 08:53:33 +0200

    refactor: minor code cleanup

--- src/main/java/org/group1/projectbackend/mapper/CommentMapper.java
+++ src/main/java/org/group1/projectbackend/mapper/CommentMapper.java
@@ -4,6 +4,8 @@ import org.group1.projectbackend.dto.comment.CommentDto;
 import org.group1.projectbackend.dto.comment.CreateCommentDto;
 import org.group1.projectbackend.dto.comment.UpdateCommentDto;
 import org.group1.projectbackend.entity.Comment;
+import org.group1.projectbackend.entity.SupportTicket;
+import org.group1.projectbackend.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,20 +18,23 @@ public class CommentMapper {
         return new CommentDto(
                 comment.getId(),
                 comment.getContent(),
-                comment.getUser().getId(),
-                comment.getUser().getUsername(),
-                comment.getTicket().getId(),
+                comment.getUser() != null ? comment.getUser().getId() : null,
+                comment.getUser() != null ? comment.getUser().getUsername() : null,
+                comment.getTicket() != null ? comment.getTicket().getId() : null,
                 comment.getCreatedAt(),
                 comment.getUpdatedAt()
         );
     }
 
-    public Comment toEntity(CreateCommentDto dto) {
+    public Comment toEntity(CreateCommentDto dto, User user, SupportTicket ticket) {
         if (dto == null) {
             return null;
         }
+
         Comment comment = new Comment();
         comment.setContent(dto.getContent());
+        comment.setUser(user);
+        comment.setTicket(ticket);
 
         return comment;
     }
